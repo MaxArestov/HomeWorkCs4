b821773ee60c69c40d4512532c6045c4e75e031e
maxim arestov <aresmax1993@gmail.com>
2022-10-25 21:31:45 +0300

    Completed Case 2

---
+++ Case2/Program.cs
@@ -0,0 +1,26 @@
+﻿// Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе. 
+//Задание должно быть выполнено в методе. В методе не должно быть вывода в консоль.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+Console.Clear();
+Console.Write("Введите число, чтобы получить сумму цифр: ");
+bool parsedNum = int.TryParse(Console.ReadLine(), out int num);
+if (!parsedNum)
+{
+    Console.WriteLine("Введены некорректные данные");
+    return;
+}
+int sum = SumOfNumbers(num);
+Console.WriteLine($"Сумма всех цифр введенного числа: {sum}");
+
+int SumOfNumbers(int x)
+{
+    int y = 0;
+    for (int i = 1; x != 0; i++)
+    {
+        y = y + (x % 10);
+        x /= 10;
+    }
+    return y;
+}
